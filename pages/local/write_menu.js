eea160063e9811973e13d7930f01c87878394803
lwannnn <47031628+lwannnn@users.noreply.github.com>
2020-12-08 23:33:35 +0800

    完成了编写菜谱详情页功能

--- pages/local/write_menu.js
+++ pages/local/write_menu.js
@@ -1,11 +1,15 @@
-// pages/local/index.js
+// pages/local/write_menu.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    menu_name: "",
+    menu_time: '',
+    menu_region: '',
+    menu_privilege: '',
+    menu_step: '',
   },
 
   /**
@@ -14,7 +18,41 @@ Page({
   onLoad: function (options) {
 
   },
-
+  getName: function (e) {
+    this.setData({
+      menu_name: e.detail.value
+    })
+  },
+  getTime: function (e) {
+    this.setData({
+      menu_time: e.detail.value
+    })
+  },
+  getRegion: function (e) {
+    this.setData({
+      menu_region: e.detail.value
+    })
+  },
+  getPrivilege: function (e) {
+    this.setData({
+      menu_privilege: e.detail.value
+    })
+  },
+  getStep: function (e) {
+    this.setData({
+      menu_step: e.detail.value
+    })
+  },
+  submit_menu: function () {
+    wx.showToast({
+      title: '发布成功！',
+    })
+    setTimeout(() => {
+      wx.navigateBack({
+        delta: 0,
+      })
+    }, 1000)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -28,27 +66,7 @@ Page({
   onShow: function () {
 
   },
-  goLocal:function(){
-    wx.navigateTo({
-      url: '../local/index',
-    })
-  },
-  goMe:function(){
-    wx.navigateTo({
-      url: '../local/me',
-    })
-  },
-  goDetail:function(){
-    wx.navigateTo({
-      url: '../local/detail',
-    })
-  },
-  toWrite:function()
-  {
-    wx.navigateTo({
-      url: '../local/write_menu',
-    })
-  },
+
   /**
    * 生命周期函数--监听页面隐藏
    */
