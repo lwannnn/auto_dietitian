6f6f099eebb9236e6f8f1969d8cb0d4c1d820677
CeaseLom <75462601+CeaseLom@users.noreply.github.com>
2020-12-11 12:17:01 +0800

    一小部分个人数据界面布局

--- pages/me/index.js
+++ pages/me/index.js
@@ -15,6 +15,12 @@ Page({
 
   },
 
+  gotoPersonalPage: function (options) {
+    wx.navigateTo({
+      url: '/pages/personaldata/index',//要跳转到的页面路径
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
